54844e18fbb67f92835e71badeca2fcf10d33c08
Owen-Rose <owen.rose@edu.sait.ca>
2023-09-27 12:02:34 -0600

    Update item.js

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,7 +1,7 @@
 export default function Item({name, quantity, category}) {
     return (
         <ul>
-            <li className="p-2 m-4 bg-slate-900 max-w-sm">
+            <li className="p-2 m-4 bg-slate-900 max-w-sm transform hover:scale-105 transition-transform ease-in duration-150 hover:bg-slate-700">
                 <h2 className="text-xl font-bold">{name}</h2>
                 <p>Buy {quantity} in {category}</p>
             </li>
